d173c115fef5a59ab2c241c4762795fcc9159359
Johy <38210128+JohyC@users.noreply.github.com>
2021-03-20 08:34:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#解决olsrd模块编译错误;
+rm -rf package/feeds/routing/olsrd/patches/012-gpsd.patch

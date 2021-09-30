8ef2ca232df30575df2adee3b521159311701583
Johy <c13340908272@outlook.com>
2021-09-30 23:37:34 +0800

    Update Build_miRouterR4AGigabit.sh

--- Build_miRouterR4AGigabit.sh
+++ Build_miRouterR4AGigabit.sh
@@ -21,7 +21,10 @@ sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/ima
 
 git clone https://github.com/walkingsky/luci-wifidog.git 
 mv luci-wifidog luci-app-wifidog 
-mv luci-app-wifidog /workdir/openwrt/package/feeds/luci/luci-app-wifidog
+
 
 ls -la
 pwd
+
+mv luci-app-wifidog /workdir/openwrt/package/feeds/luci/luci-app-wifidog
+
